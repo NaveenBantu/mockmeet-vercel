a8840c5f912fa2a507127f7696d52b7e4f465535
NaveenBantu <naveen.bantu1@gmail.com>
2023-05-02 08:15:22 +0200

    Add Header component

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/button";
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>MockMeet</h1>
+    </>
+  );
+};
+
+export default Header;
