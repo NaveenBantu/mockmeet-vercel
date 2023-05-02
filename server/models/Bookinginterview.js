0f0b27b74b7398d56325eaf03573f09ded94c972
Saiteja-21 <saitejakatkam123@gmail.com>
2023-05-02 10:38:58 +0530

    added Bookinginterview model

---
+++ server/models/Bookinginterview.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const BookedInterview=new mongoose.Schema({
+    mock_id:{
+        type:String,
+        required:true   
+      },
+      score:{
+        type:Number,
+        required:true   
+      },
+    student_id:{
+      type:String,
+      required:true   
+    },
+    interviewer_id:{
+        type:String,
+        required:true
+    },
+    iscompleted:{
+        type:Boolean,
+        default:false   
+      },
+    bookingDate:{
+        type:Date,
+        required:true
+    }
+    
+    
+
+})
+export default Interviews = mongoose.model('Interviews',BookedInterview);
