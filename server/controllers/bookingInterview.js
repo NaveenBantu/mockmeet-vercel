6123e01ca532f927772f552b6d2b03c5c49bdf12
NaveenBantu <naveen.bantu1@gmail.com>
2023-05-04 08:53:34 +0200

    Add data-mapping to interviewer for modifying available dates

--- server/controllers/bookingInterview.js
+++ server/controllers/bookingInterview.js
@@ -1,10 +1,22 @@
 import Interview from "../models/Bookinginterview.js";
+import Interviewer from "../models/Interviewer.js";
 
 //create new interview
 export const createInterview = async (req, res, next) => {
   const newInterview = new Interview(req.body);
   try {
     const savedInterview = await newInterview.save();
+    // Delete the booked date from interviewer available dates
+    try {
+      await Interviewer.findOneAndUpdate(
+        { _id: savedInterview.interviewer_id },
+        {
+          $pull: { availableDates: savedInterview.bookingDate },
+        }
+      );
+    } catch (err) {
+      next(err);
+    }
     res.status(200).json(savedInterview);
   } catch (err) {
     next(err);
@@ -26,6 +38,17 @@ export const updateInterview = async (req, res, next) => {
 export const deleteInterview = async (req, res, next) => {
   try {
     const deletedInterview = await Interview.findOneAndDelete(req.params.id);
+    // Add the booking date to interviewer available dates after deleting the interview
+    try {
+      await Interviewer.findOneAndUpdate(
+        { _id: deletedInterview.interviewer_id },
+        {
+          $push: { availableDates: deletedInterview.bookingDate },
+        }
+      );
+    } catch (err) {
+      next(err);
+    }
     res.status(200).json("Interview has been removed");
   } catch (err) {
     next(err);
