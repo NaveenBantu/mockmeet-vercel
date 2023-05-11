833a2bd1ec003e7afe443dfdb0e8e934197e5593
KolliSai <saik98187@gmail.com>
2023-05-11 11:03:16 +0530

    added filter methods to userController for getAllUsers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -51,7 +51,100 @@ export const getUser = async (req, res, next) => {
 // Get all Users
 export const getUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
+    // we can search using these queries
+    // like
+    //api/users?username='something'&email='something'
+    const {
+      username,
+      name,
+      email,
+      phone,
+      createdAt,
+      updatedAt,
+      _id,
+      numericFilters,
+      sort,
+      fields,
+    } = req.query;
+    const queryObj = {};
+    if (username) {
+      queryObj.username = username;
+    }
+    if (name) {
+      queryObj.name = name;
+    }
+    if (email) {
+      queryObj.email = email;
+    }
+    if (phone) {
+      queryObj.phone = phone;
+    }
+    if (createdAt) {
+      queryObj.createdAt = createdAt;
+    }
+    if (updatedAt) {
+      queryObj.updatedAt = updatedAt;
+    }
+    if (_id) {
+      queryObj._id = _id;
+    }
+
+    // to search numberqueries like overall_score
+    // we can search
+    //api/users?numericFilters=overall_score>5 || <5 || we can use < <= = > >= operators
+
+    if (numericFilters) {
+      const operatorMap = {
+        ">": "$gt",
+        ">=": "$gte",
+        "=": "$eq",
+        "<": "$lt",
+        "<=": "$lte",
+      };
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+      let filters = numericFilters.replace(
+        regEx,
+        (match) => `-${operatorMap[match]}-`
+      );
+      const options = ["overall_score"];
+      filters = filters.split(",").forEach((item) => {
+        const [field, operator, value] = item.split("-");
+        if (options.includes(field)) {
+          queryObj[field] = { [operator]: Number(value) };
+        }
+      });
+    }
+
+    let result = User.find(queryObj);
+
+    // to sort
+    // /api/users?sort =name,-overall_score
+    // sort
+    if (sort) {
+      const sortList = sort.split(",").join(" ");
+      result.sort(sortList);
+    } else {
+      result.sort("createdAt");
+    }
+    // fields
+    //select only particular fields
+    // /api/users?fields=name,email,-password
+
+    if (fields) {
+      const fieldsList = fields.split(",").join(" ");
+      result.select(fieldsList);
+    }
+    // limit and skip
+    // we can limit the documents using limit per page
+    // /api/users?limit=4&page=2
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
+    const users = await result;
+
     res.status(200).json(users);
   } catch (err) {
     next(err);
@@ -69,4 +162,4 @@ export const getUserInterviews = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
