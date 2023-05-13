7f0b19abdf8bb0b00b3572c2f93fdac729ff360b
NaveenBantu <naveen.bantu1@gmail.com>
2023-05-13 15:01:12 +0200

    Update interview card

--- client/src/components/InterviewCard/index.jsx
+++ client/src/components/InterviewCard/index.jsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 import moment from "moment";
+import { Badge } from "@chakra-ui/react";
+import LoadingSpinner from "../LoadingSpinner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const InterviewCard = ({ title, date, time, interviewers }) => {
-  const [disable, setDisable] = useState(true);
+const InterviewCard = ({ id, title, date, time, interviewer }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     let startTime = moment(`${date} ${time}`, "MM/DD/YYYY HH:mm:ss");
@@ -18,20 +26,38 @@ const InterviewCard = ({ title, date, time, interviewers }) => {
     }
   }, []);
 
+  const handleDeleteInterview = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.delete(
+        `${import.meta.env.VITE_REACT_API_URL}/bookinginterviews/${id}`
+      );
+      setData(response.data);
+      navigate("/upcoming-interviews");
+    } catch (error) {
+      setError(error);
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <div className={styles.container}>
-      <h4 className={styles.title}>{title}</h4>
-      <div className={styles.details}>
-        <>
-          <p>Date: {date.replaceAll("/", "-")}</p>
-          <p>Time: {time.slice(0, 5)}</p>
-          <p>By:{interviewers.join(", ")}</p>
-          <button className={styles.join} disabled={disable}>
-            Join
-          </button>
-        </>
-      </div>
-    </div>
+    <center>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className={styles.container}>
+          <h4 className={styles.title}>{title}</h4>
+          <div className={styles.details}>
+            <>
+              <h4>By:{interviewer}</h4>
+              <Badge>{new Date(date).toLocaleString()}</Badge>
+            </>
+          </div>
+          <button onClick={handleDeleteInterview}>Delete</button>
+        </div>
+      )}
+    </center>
   );
 };
 
