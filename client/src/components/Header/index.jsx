2e7819321f9ff8d0a754a03f65266297eae9d13b
NaveenBantu <naveen.bantu1@gmail.com>
2023-06-25 09:07:25 +0200

    Remove material ui icons

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import styles from "../Header/styles.module.css";
 import GridViewIcon from "../../../node_modules/@mui/icons-material/GridView";
 // import GridViewIcon from "@mui/icons-material/GridView";
+import { FaSlackHash } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 import { Heading } from "@chakra-ui/react";
--- client/src/pages/resources/Accordion.jsx
+++ client/src/pages/resources/Accordion.jsx
@@ -1,61 +1,61 @@
 /* eslint-disable react/prop-types */
 // import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  List,
-  ListItem,
-  ListItemText,
-  Container,
-  Typography,
-} from "../../../node_modules/@mui/material";
-import ExpandMoreIcon from "../../../node_modules/@mui/icons-material/ExpandMore";
+// import {
+//   Accordion,
+//   AccordionDetails,
+//   AccordionSummary,
+//   List,
+//   ListItem,
+//   ListItemText,
+//   Container,
+//   Typography,
+// } from "../../../node_modules/@mui/material";
+// import ExpandMoreIcon from "../../../node_modules/@mui/icons-material/ExpandMore";
 
 function Accordion_list(props) {
   let obj = props.data;
   let title = props.title;
 
-  const accordions = Object.keys(obj).map((key) => (
-    <Accordion
-      key={key}
-      sx={{
-        mb: "5px",
-      }}
-    >
-      <AccordionSummary
-        sx={{
-          backgroundColor: "black",
-          color: "#FAA621",
-        }}
-        expandIcon={<ExpandMoreIcon sx={{ color: "#FAA621" }} />}
-        aria-controls={`${key}-content`}
-        id={`${key}-header`}
-      >
-        {key}
-      </AccordionSummary>
-      <AccordionDetails>
-        <List>
-          {obj[key].map((ele) => (
-            <ListItem
-              key={ele.text}
-              button
-              component={Link}
-              to={ele.link}
-              target="_blank"
-            >
-              <ListItemText primary={ele.text} />
-            </ListItem>
-          ))}
-        </List>
-      </AccordionDetails>
-    </Accordion>
-  ));
+  // const accordions = Object.keys(obj).map((key) => (
+  //   <Accordion
+  //     key={key}
+  //     sx={{
+  //       mb: "5px",
+  //     }}
+  //   >
+  //     <AccordionSummary
+  //       sx={{
+  //         backgroundColor: "black",
+  //         color: "#FAA621",
+  //       }}
+  //       expandIcon={<ExpandMoreIcon sx={{ color: "#FAA621" }} />}
+  //       aria-controls={`${key}-content`}
+  //       id={`${key}-header`}
+  //     >
+  //       {key}
+  //     </AccordionSummary>
+  //     <AccordionDetails>
+  //       <List>
+  //         {obj[key].map((ele) => (
+  //           <ListItem
+  //             key={ele.text}
+  //             button
+  //             component={Link}
+  //             to={ele.link}
+  //             target="_blank"
+  //           >
+  //             <ListItemText primary={ele.text} />
+  //           </ListItem>
+  //         ))}
+  //       </List>
+  //     </AccordionDetails>
+  //   </Accordion>
+  // ));
 
   return (
     <>
-      <Container maxWidth={"sm"} sx={{ mt: "20px" }}>
+      {/* <Container maxWidth={"sm"} sx={{ mt: "20px" }}>
         <center>
           <Typography
             sx={{
@@ -69,7 +69,8 @@ function Accordion_list(props) {
           </Typography>
         </center>
         {accordions}
-      </Container>
+      </Container> */}
+      <h1>Accordian List</h1>
     </>
   );
 }
