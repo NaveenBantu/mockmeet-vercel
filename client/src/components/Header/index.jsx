6b1c6becf947d0fb2bd6d8b3ce81939aed8baa6c
NaveenBantu <naveen.bantu1@gmail.com>
2023-06-25 09:21:19 +0200

    Remove material grid icon

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import styles from "../Header/styles.module.css";
-import GridViewIcon from "../../../node_modules/@mui/icons-material/GridView";
+// import GridViewIcon from "../../../node_modules/@mui/icons-material/GridView";
 // import GridViewIcon from "@mui/icons-material/GridView";
 import { FaSlackHash } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
@@ -17,7 +17,7 @@ function Header({ user }) {
   return (
     <div className={styles.container}>
       <div className={styles.tabbar}>
-        {isSignedIn && <GridViewIcon onClick={handleChange} />}
+        {isSignedIn && <FaSlackHash size="2rem" onClick={handleChange} />}
       </div>
       <Heading alignSelf="center">MockMeet</Heading>
       <div className={styles.icon}>{user}</div>
